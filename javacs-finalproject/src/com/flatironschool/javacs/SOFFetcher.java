63c922a34bd002fab720df0ba949c90b45ec18b9
Chohee Kim <kch5559@gmail.com>
2016-07-31 11:49:46 -0700

    Add stackoverflow fetcher

---
+++ javacs-finalproject/src/com/flatironschool/javacs/SOFFetcher.java
@@ -0,0 +1,123 @@
+
+package com.flatironschool.javacs;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+
+public class SOFFetcher {
+	private long lastRequestTime = -1;
+	private long minInterval = 1000;
+
+
+
+	public Document getDocument(String url) throws IOException {
+
+		sleepIfNeeded();
+
+		Connection conn = Jsoup.connect(url);
+		return conn.get();
+	}
+
+
+
+	/**
+	 * Fetches and parses a URL string, returning a list of paragraph elements.
+	 *
+	 * @param url
+	 * @return
+	 * @throws IOException
+	 */
+	public Elements fetchStackoverflow(Document doc) throws IOException {
+
+
+		Elements body = doc.select("body");
+
+		//Elements body = doc.select("body content a");
+
+		return body;
+
+	}
+
+	public Elements readStackoverflow(Document doc, String className) throws IOException {
+
+
+		Elements question = doc.getElementsByClass(className).select("p");
+		return question;
+	}
+
+	public Elements allRelatedAndLinked(Document doc) throws IOException {
+
+		Elements all = new Elements();
+		all.addAll(readHeader(doc));
+		all.addAll(relatedAndLinked(doc));
+
+		return all;
+	}
+
+	private Elements readHeader(Document doc) throws IOException {
+
+		Elements headers = doc.getElementById("question-header").select("h1");
+		return headers;
+	}
+
+	private Elements relatedAndLinked(Document doc) throws IOException {
+
+		Elements linked = doc.getElementsByClass("question-hyperlink");
+		return linked;
+	}
+
+
+
+	/**
+	 * Rate limits by waiting at least the minimum interval between requests.
+	 */
+	private void sleepIfNeeded() {
+		if (lastRequestTime != -1) {
+			long currentTime = System.currentTimeMillis();
+			long nextRequestTime = lastRequestTime + minInterval;
+			if (currentTime < nextRequestTime) {
+				try {
+					//System.out.println("Sleeping until " + nextRequestTime);
+					Thread.sleep(nextRequestTime - currentTime);
+				} catch (InterruptedException e) {
+					System.err.println("Warning: sleep interrupted in fetchWikipedia.");
+				}
+			}
+		}
+		lastRequestTime = System.currentTimeMillis();
+	}
+
+	public static void main(String[] arsg) {
+
+		String url = "http://stackoverflow.com/questions/12672428/css-max-width-wont-shrink?noredirect=1&lq=1";
+
+		SOFFetcher test = new SOFFetcher();
+
+
+		Elements all = new Elements();
+		try {
+			Elements header = test.relatedAndLinked(test.getDocument(url));
+			Elements linked = test.relatedAndLinked(test.getDocument(url));
+
+			all.addAll(header);
+			all.addAll(linked);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		for(Element el : all) {
+			System.out.println(el.text());
+		}
+
+	}
+}
